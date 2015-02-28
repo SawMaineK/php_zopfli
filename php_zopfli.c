790c7b38e84da8ee7d7e7ce6d216c3f9d7d29d1f
Benjamin Carl <opensource@clickalicious.de>
2015-02-28 23:22:26 +0100

    Updated LICENSE // added .editorconfig // prepared changelog

--- php_zopfli.c
+++ php_zopfli.c
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *  Copyright (c) 2013-2015 clickalicious GmbH (i.G.) - Benjamin Carl
+ *  Copyright (c) 2013 - 2015 clickalicious GmbH (i.G.) - Benjamin Carl
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
